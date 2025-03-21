e3ae3b5943b49850f109e519a81f3dfb6de314da
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-21 03:41:33 +0000

    cambios

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [name, resultName] = useState()
+	console.log(name)
 	return (
-		<div className="text-center d-flex justify-content-center">
+		<div className="text-center d-flex justify-content-center align-items-center">
+			<div className=" d-flex flex-column gap-2 bg-danger-subtle justify-content-center align-items-center" style={{ maxWith: "100%" }}>
+				<h4 className="text-success">{name}</h4>
+				<input type="text" className="form-control"
+					onChange={(event) => {
+						resultName(event.target.value)
+					}}
+					value={name}
+				/>
+
+			</div>
 			<div className="card" style={{ width: "18rem" }}>
 				<ul className="list-group list-group-flush">
 					<li className="list-group-item">An item</li>
@@ -19,4 +31,4 @@ const Home = () => {
 	);
 };
 
-export default Home;
+export default Home; 
