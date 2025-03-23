472808b6bb11b48def9c497ce3ff00351dddd113
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-23 18:57:08 +0000

    ultimos cambios

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -24,30 +24,30 @@ const Home = () => {
 	return (
 		<div className="card d-flex bg-secondary-subtle card-container">
 			<div className=" card-form text-center d-flex flex-column mb-3 justify-content-center align-items-center">
-				<h1 className="text-success">todos</h1>
+				<h1 className="text-success title-container">todos</h1>
 				<div
 					className="d-flex"
 					style={{ width: "18rem" }}
 				>
 					<input
 						type="text"
-						className="form-control"
+						className="form-control mb-3"
 						onChange={(event) => setTaskInput(event.target.value)}
 						onKeyDown={handleKeyDown} // Detecta la tecla Enter
 						value={taskInput}
 						placeholder="Escribe una tarea y presiona Enter"
 					/>
 				</div>
-				<div className="card-items" style={{ width: "18rem" }}>
-					<ul className="list-group list-group-flush">
+				<div className="card-items d-flex flex-column " style={{ width: "18rem" }}>
+					<ul className="list-group list-group-flush ">
 						{tasks.length === 0 ? (
-							<li className="list-group-item">No hay tareas, añadir tareas</li>
+							<li className="list-group-item ">No hay tareas, añadir tareas</li>
 						) : (
 							tasks.map((task, index) => (
-								<li key={index} className="list-group-item task-item">
+								<li key={index} className="list-group-item task-item  d-flex">
 									{task}
 									<span
-										className="delete-icon"
+										className="delete-icon fs-4 "
 										onClick={() => handleDelete(index)}
 									>
 										x
@@ -56,7 +56,7 @@ const Home = () => {
 							))
 						)}
 					</ul>
-					<div className="card-footer bg-danger-subtle">
+					<div className="card-footer bg-danger-subtle text-secondary justify-content-start d-flex" >
 						{tasks.length} {tasks.length === 1 ? "item left" : "items left"}
 					</div>
 				</div>
